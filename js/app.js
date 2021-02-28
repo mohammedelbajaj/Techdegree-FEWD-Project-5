c1800eb5ad1b968fcd0889dc9019d4e423ea9b37
MohaElba <mohammed.elbajaj@gmail.com>
2021-02-28 14:14:24 +0100

    Finalize project 5

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');​
-
+// Searchfilter
+function search_thumbnail() { 
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase(); 
+    let x = document.getElementsByTagName('a'); 
+    
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].getAttribute('data-caption').toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="";				 
+            } 
+        } 
+    } 
